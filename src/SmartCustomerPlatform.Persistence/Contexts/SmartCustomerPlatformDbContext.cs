62439e4126bdff4e9a71b7d68ac987f834902100
iklimcelebi <iklimcelebi1@icloud.com>
2026-08-03 23:49:12 +0300

    feat(persistence): setup Entity Framework Core and DbContext

---
+++ src/SmartCustomerPlatform.Persistence/Contexts/SmartCustomerPlatformDbContext.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using SmartCustomerPlatform.Domain.Entities;
+
+namespace SmartCustomerPlatform.Persistence.Contexts;
+
+public class SmartCustomerPlatformDbContext : DbContext //inheritance: inherits from DbContext class provided by Entity Framework Core
+{
+    public SmartCustomerPlatformDbContext(
+        DbContextOptions<SmartCustomerPlatformDbContext> options)// <> generic type
+        : base(options) //calls the constructor of DBContext 
+    {
+    }
+
+    public DbSet<Customer> Customers => Set<Customer>(); //creates a DBtable named Customer, also creates getter and setter
+
+    public DbSet<Subscription> Subscriptions => Set<Subscription>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(SmartCustomerPlatformDbContext).Assembly); // applies all entity configurations defined in the assembly where SmartCustomerPlatformDbContext is located. 
+
+    }
+}
